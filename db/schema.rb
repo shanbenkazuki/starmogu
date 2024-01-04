bfe62b9e52fd440f17d224f5e6d9021d52237080
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-01-04 22:16:52 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_04_095623) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_04_131118) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "user_name"
+    t.string "username", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
