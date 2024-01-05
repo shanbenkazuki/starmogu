c9a107e171e8b25cfff27d9774efebadea59e5f2
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-01-05 13:32:09 +0900

    commit

--- app/javascript/components/ScoreModal.js
+++ app/javascript/components/ScoreModal.js
@@ -6,7 +6,8 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 const ScoreModal = ({ score, isOpen, onClose, level }) => {
   const levelText = ["初級", "中級", "上級"][level];
-  const tweetText = `私の${levelText}のスコアは ${score} 点です！`;
+  const pageUrl = "https://www.canva.com/design/DAF4_JtN6F0/n1EoORt16aRrpw9ND6tnBg/view?utm_content=DAF4_JtN6F0&utm_campaign=designshare&utm_medium=link&utm_source=editor";
+  const tweetText = `私の${levelText}のスコアは ${score} 点です！ #ミニアプリウィーク ${pageUrl}`;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
 
   return (
