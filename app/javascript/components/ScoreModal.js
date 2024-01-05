0f107a6727d245512b73f2c54ee4f40ac6d72078
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-01-05 13:38:43 +0900

    公開用リンク修正

--- app/javascript/components/ScoreModal.js
+++ app/javascript/components/ScoreModal.js
@@ -6,7 +6,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 const ScoreModal = ({ score, isOpen, onClose, level }) => {
   const levelText = ["初級", "中級", "上級"][level];
-  const pageUrl = "https://www.canva.com/design/DAF4_JtN6F0/n1EoORt16aRrpw9ND6tnBg/view?utm_content=DAF4_JtN6F0&utm_campaign=designshare&utm_medium=link&utm_source=editor";
+  const pageUrl = "https://www.canva.com/design/DAF4_Jx8dDA/Yq2tsXI1qtlQPXy62iOwOA/view?utm_content=DAF4_Jx8dDA&utm_campaign=designshare&utm_medium=link&utm_source=editor";
   const tweetText = `私の${levelText}のスコアは ${score} 点です！ #ミニアプリウィーク ${pageUrl}`;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
 
