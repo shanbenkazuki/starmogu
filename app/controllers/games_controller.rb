37b4fc03b2beac1cac0b6d6e10d338309940514c
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-01-05 22:37:57 +0900

    ランキングの重複をなくす

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,12 +17,21 @@ class GamesController < ApplicationController
   end
 
   def fetch_ranking
-    ranking = Score.includes(:user).where(level: 2).order(score: :desc).limit(10)
+    ranking = Score.where(level: 2)
+    .select('MAX(scores.score) as max_score, scores.user_id')
+    .group(:user_id)
+    .order('max_score DESC')
+    .limit(10)
   
-    ranking_data = ranking.map do |score|
-      { username: score.user.username, score: score.score, level: "上級" }
+    ranking_data = ranking.map do |record|
+      user = User.find(record.user_id)
+      {
+        username: user.username,
+        score: record.max_score,
+        level: "上級"
+      }
     end
-  
+    
     render json: ranking_data
   end
 end
